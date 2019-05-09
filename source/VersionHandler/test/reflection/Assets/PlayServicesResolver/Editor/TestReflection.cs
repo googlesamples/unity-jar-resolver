googlesamples/unity-jar-resolver

commit ea3e8d815e6d0148af87ece3deefdb9be6b2dc1f

Support for Unity 2019 Android SDK and JDK install locations
Unity 2019 adds support for installation of the Android SDK and JDK in
the AndroidPlayer directory making it easier for developers to configure
the Android build environment.  Unfortunately, Unity does not update
the JdkPath and AndroidSdkRoot editor preferences when the Android SDK
and JDK are installed with the AndroidPlayer.

This change searches for the Android SDK and JDK if JdkPath and
AndroidSdkRoot are not set.

Fixes #188

Bug: 132072681
Change-Id: If01f642ff81570d9c28552fab6957bc88cf4dac1

source/VersionHandler/src/VersionHandler.cs
@@ -494,7 +494,15 @@ public class VersionHandler {
     public static object InvokeMethod(
             Type type, object objectInstance, string methodName,
             object[] args, Dictionary<string, object> namedArgs = null) {
-        MethodInfo method = type.GetMethod(methodName);
+        Type[] argTypes = null;
+        if (args != null && args.Length > 0) {
+            argTypes = new Type[args.Length];
+            for (int i = 0; i < args.Length; ++i) {
+                argTypes[i] = args[i].GetType();
+            }
+        }
+        MethodInfo method = argTypes != null ?
+            type.GetMethod(methodName, argTypes) : type.GetMethod(methodName);
         ParameterInfo[] parameters = method.GetParameters();
         int numParameters = parameters.Length;
         object[] parameterValues = new object[numParameters];

source/PlayServicesResolver/src/JavaUtilities.cs
@@ -49,19 +49,25 @@ namespace GooglePlayServices {
         private static Version MinimumJdkVersion = new Version("1.8");
 
         /// <summary>
-        /// Get the JDK path (JAVA_HOME) configured in the Unity editor.
-        /// </summary>
-        private static string EditorJavaHome {
-            get { return UnityEditor.EditorPrefs.GetString("JdkPath"); }
-        }
-
-        /// <summary>
         /// Find the JDK path (JAVA_HOME) either configured in the Unity editor or via the JAVA_HOME
         /// environment variable.
         /// </summary>
         private static string JavaHome {
             get {
-                var javaHome = EditorJavaHome;
+                var javaHome = UnityEditor.EditorPrefs.GetString("JdkPath");
+                // Unity 2019.x added installation of the JDK in the AndroidPlayer directory
+                // so fallback to searching for it there.
+                if (String.IsNullOrEmpty(javaHome) || EditorPrefs.GetBool("JdkUseEmbedded")) {
+                    var androidPlayerDir = PlayServicesResolver.AndroidPlaybackEngineDirectory;
+                    if (!String.IsNullOrEmpty(androidPlayerDir)) {
+                        var platformDir = UnityEngine.Application.platform.ToString().Replace(
+                            "Editor", "").Replace("OSX", "MacOS");
+                        var openJdkDir = Path.Combine(Path.Combine(Path.Combine(
+                            androidPlayerDir, "Tools"), "OpenJDK"), platformDir);
+                        if (Directory.Exists(openJdkDir)) javaHome = openJdkDir;
+                    }
+                }
+                // If the JDK stil isn't found, check the environment.
                 if (String.IsNullOrEmpty(javaHome)) {
                     javaHome = Environment.GetEnvironmentVariable(JAVA_HOME);
                 }

source/PlayServicesResolver/src/PlayServicesResolver.cs
@@ -608,7 +608,19 @@ namespace GooglePlayServices {
         /// Get the Android SDK directory.
         /// </summary>
         public static string AndroidSdkRoot {
-            get { return EditorPrefs.GetString("AndroidSdkRoot"); }
+            get {
+                var sdkPath = EditorPrefs.GetString("AndroidSdkRoot");
+                // Unity 2019.x added installation of the Android SDK in the AndroidPlayer directory
+                // so fallback to searching for it there.
+                if (String.IsNullOrEmpty(sdkPath) || EditorPrefs.GetBool("SdkUseEmbedded")) {
+                    var androidPlayerDir = AndroidPlaybackEngineDirectory;
+                    if (!String.IsNullOrEmpty(androidPlayerDir)) {
+                        var androidPlayerSdkDir = Path.Combine(androidPlayerDir, "SDK");
+                        if (Directory.Exists(androidPlayerSdkDir)) sdkPath = androidPlayerSdkDir;
+                    }
+                }
+                return sdkPath;
+            }
         }
 
         /// <summary>
@@ -636,6 +648,22 @@ namespace GooglePlayServices {
         /// Event which is fired when the Android SDK root changes.
         /// </summary>
         public static event EventHandler<AndroidSdkRootChangedArgs> AndroidSdkRootChanged;
+
+        /// <summary>
+        /// Get the Android playback engine directory.
+        /// </summary>
+        /// <returns>Get the playback engine directory.</returns>
+        public static string AndroidPlaybackEngineDirectory {
+            get {
+                try {
+                    return (string)VersionHandler.InvokeStaticMethod(
+                        typeof(BuildPipeline), "GetPlaybackEngineDirectory",
+                        new object[] { BuildTarget.Android, BuildOptions.None });
+                } catch (Exception) {
+                    return null;
+                }
+            }
+        }
 
         /// <summary>
         /// Initializes the <see cref="GooglePlayServices.PlayServicesResolver"/> class.

commit e71269b2b7391efa72fe9675f706376f11a1e123

Fixed InvokeMethod() regression introduced by ea3e8d815
InvokeMethod() was modified in ea3e8d815 to support method overloads
distiguished by type.  However, this change broke the lookup of methods
with named arguments.

Fixes #209
Bug: 132342024

Change-Id: I151fe5c893fbbc9b7feb0cf371b0e693a91e17bc

build.gradle
@@ -1286,6 +1286,13 @@ createUnityTestBatchAndNonBatch(
   new File("source/VersionHandlerImpl/test/activation"), [])
 
 createUnityTestBatchAndNonBatch(
+  "testVersionHandlerReflection",
+  ("Imports the plugin into a Unity project and tests reflection " +
+   "methods."),
+  [buildPlugin],
+  new File("source/VersionHandler/test/reflection"), [])
+
+createUnityTestBatchAndNonBatch(
   "testAndroidResolverAsyncResolve",
   ("Imports the plugin into a Unity project, runs Android resolution with " +
    "a combination of dependencies added via the programmatic API and via " +

source/VersionHandler/src/VersionHandler.cs
@@ -494,35 +494,56 @@ public class VersionHandler {
     public static object InvokeMethod(
             Type type, object objectInstance, string methodName,
             object[] args, Dictionary<string, object> namedArgs = null) {
-        Type[] argTypes = null;
-        if (args != null && args.Length > 0) {
-            argTypes = new Type[args.Length];
-            for (int i = 0; i < args.Length; ++i) {
-                argTypes[i] = args[i].GetType();
-            }
-        }
-        MethodInfo method = argTypes != null ?
-            type.GetMethod(methodName, argTypes) : type.GetMethod(methodName);
-        ParameterInfo[] parameters = method.GetParameters();
-        int numParameters = parameters.Length;
-        object[] parameterValues = new object[numParameters];
-        int numPositionalArgs = args != null ? args.Length : 0;
-        foreach (var parameter in parameters) {
-            int position = parameter.Position;
-            if (position < numPositionalArgs) {
-                parameterValues[position] = args[position];
-                continue;
-            }
-            object namedValue = parameter.RawDefaultValue;
-            if (namedArgs != null) {
-                object overrideValue;
-                if (namedArgs.TryGetValue(parameter.Name, out overrideValue)) {
-                    namedValue = overrideValue;
+        object[] parameterValues = null;
+        int numberOfPositionalArgs = args != null ? args.Length : 0;
+        int numberOfNamedArgs = namedArgs != null ? namedArgs.Count : 0;
+        MethodInfo foundMethod = null;
+        foreach (var method in type.GetMethods()) {
+            if (method.Name != methodName) continue;
+            var parameters = method.GetParameters();
+            int numberOfParameters = parameters.Length;
+            parameterValues = new object[numberOfParameters];
+            int matchedPositionalArgs = 0;
+            int matchedNamedArgs = 0;
+            bool matchedAllRequiredArgs = true;
+            foreach (var parameter in parameters) {
+                var parameterType = parameter.ParameterType;
+                int position = parameter.Position;
+                if (position < numberOfPositionalArgs) {
+                    var positionalArg = args[position];
+                    // If the parameter type doesn't match, ignore this method.
+                    if (positionalArg != null && parameterType != positionalArg.GetType()) break;
+                    parameterValues[position] = positionalArg;
+                    matchedPositionalArgs ++;
+                } else if (parameter.RawDefaultValue != DBNull.Value) {
+                    object namedValue = parameter.RawDefaultValue;
+                    if (numberOfNamedArgs > 0) {
+                        object namedArg;
+                        if (namedArgs.TryGetValue(parameter.Name, out namedArg)) {
+                            // If the parameter type doesn't match, ignore this method.
+                            if (namedArg != null && parameterType != namedArg.GetType()) break;
+                            namedValue = namedArg;
+                            matchedNamedArgs ++;
+                        }
+                    }
+                    parameterValues[position] = namedValue;
+                } else {
+                    matchedAllRequiredArgs = false;
+                    break;
                 }
             }
-            parameterValues[position] = namedValue;
+            // If all arguments were consumed by the method, we've found a match.
+            if (matchedAllRequiredArgs &&
+                matchedPositionalArgs == numberOfPositionalArgs &&
+                matchedNamedArgs == numberOfNamedArgs) {
+                foundMethod = method;
+                break;
+            }
         }
-        return method.Invoke(objectInstance, parameterValues);
+        if (foundMethod == null) {
+            throw new Exception(String.Format("Method {0}.{1} not found", type.Name, methodName));
+        }
+        return foundMethod.Invoke(objectInstance, parameterValues);
     }
 }
 

source/VersionHandler/test/reflection/Assets/PlayServicesResolver/Editor/TestReflection.cs
@@ -1,0 +1,239 @@
+// <copyright file="TestReflection.cs" company="Google Inc.">
+// Copyright (C) 2019 Google Inc. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+
+using Google;
+
+// Class used to test method invocation.
+public class Greeter {
+    private string name;
+
+    public Greeter(string name) {
+        this.name = name;
+    }
+
+    public static string GenericHello() {
+        return "Hello";
+    }
+
+    public static string GenericHelloWithCustomerName(string customerName) {
+        return String.Format("{0} {1}", GenericHello(), customerName);
+    }
+
+    public static string GenericHelloWithCustomerName(int customerId) {
+        return String.Format("{0} customer #{1}", GenericHello(), customerId);
+    }
+
+    public static string GenericHelloWithPronoun(string pronoun = "There") {
+        return String.Format("{0} {1}", GenericHello(), pronoun);
+    }
+
+    public static string GenericHelloWithCustomerNameAndPronoun(string customerName,
+                                                                string pronoun = "There") {
+        return String.Format("{0} {1}", GenericHelloWithPronoun(pronoun: pronoun), customerName);
+    }
+
+    private string MyNameIs() {
+        return String.Format(", my name is {0}", name);
+    }
+
+    public string Hello() {
+        return Greeter.GenericHello() + MyNameIs();
+    }
+
+    public string HelloWithCustomerName(string customerName) {
+        return Greeter.GenericHelloWithCustomerName(customerName) + MyNameIs();
+    }
+
+    public string HelloWithCustomerNameAndPronoun(string customerName,
+                                                  string pronoun = "There") {
+        return Greeter.GenericHelloWithCustomerNameAndPronoun(customerName,
+                                                              pronoun: pronoun) + MyNameIs();
+    }
+}
+
+[UnityEditor.InitializeOnLoad]
+public class TestReflection {
+
+    /// <summary>
+    /// Test all reflection methods.
+    /// </summary>
+    static TestReflection() {
+        // Disable stack traces for more condensed logs.
+        UnityEngine.Application.stackTraceLogType = UnityEngine.StackTraceLogType.None;
+
+        // Run tests.
+        var failures = new List<string>();
+        foreach (var test in new Func<bool>[] {
+                    TestFindClassWithAssemblyName,
+                    TestFindClassWithoutAssemblyName,
+                    TestInvokeStaticMethodWithNoArgs,
+                    TestInvokeStaticMethodWithStringArg,
+                    TestInvokeStaticMethodWithIntArg,
+                    TestInvokeStaticMethodWithNamedArgDefault,
+                    TestInvokeStaticMethodWithNamedArg,
+                    TestInvokeStaticMethodWithArgAndNamedArgDefault,
+                    TestInvokeStaticMethodWithArgAndNamedArg,
+                    TestInvokeInstanceMethodWithNoArgs,
+                    TestInvokeInstanceMethodWithNamedArgDefault,
+                    TestInvokeInstanceMethodWithNamedArg
+                 }) {
+            var testName = test.Method.Name;
+            Exception exception = null;
+            bool succeeded = false;
+            try {
+                UnityEngine.Debug.Log(String.Format("Running test {0}", testName));
+                succeeded = test();
+            } catch (Exception ex) {
+                exception = ex;
+                succeeded = false;
+            }
+            if (succeeded) {
+                UnityEngine.Debug.Log(String.Format("{0}: PASSED", testName));
+            } else {
+                UnityEngine.Debug.LogError(String.Format("{0} ({1}): FAILED", testName, exception));
+                failures.Add(testName);
+            }
+        }
+
+        if (failures.Count > 0) {
+            UnityEngine.Debug.Log("Test failed");
+            foreach (var testName in failures) {
+                UnityEngine.Debug.Log(String.Format("{0}: FAILED", testName));
+            }
+            UnityEditor.EditorApplication.Exit(1);
+        }
+        UnityEngine.Debug.Log("Test passed");
+        UnityEditor.EditorApplication.Exit(0);
+    }
+
+    // Test searching for a class when specifying the assembly name.
+    static bool TestFindClassWithAssemblyName() {
+        var expectedType = typeof(UnityEditor.EditorApplication);
+        var foundType = VersionHandler.FindClass("UnityEditor", "UnityEditor.EditorApplication");
+        if (expectedType != foundType) {
+            UnityEngine.Debug.LogError(String.Format("Unexpected type {0} vs {1}", foundType,
+                                                     expectedType));
+            return false;
+        }
+        return true;
+    }
+
+    // Test searching for a class without specifying the assembly name.
+    static bool TestFindClassWithoutAssemblyName() {
+        var expectedType = typeof(UnityEditor.EditorApplication);
+        var foundType = VersionHandler.FindClass(null, "UnityEditor.EditorApplication");
+        if (expectedType != foundType) {
+            UnityEngine.Debug.LogError(String.Format("Unexpected type {0} vs {1}", foundType,
+                                                     expectedType));
+            return false;
+        }
+        return true;
+    }
+
+    static bool CheckValue(string expected, string value) {
+        if (value != expected) {
+            UnityEngine.Debug.LogError(String.Format("Unexpected value {0} vs {1}", value,
+                                                     expected));
+            return false;
+        }
+        return true;
+    }
+
+    // Invoke a static method with no arguments.
+    static bool TestInvokeStaticMethodWithNoArgs() {
+        return CheckValue("Hello",
+                          (string)VersionHandler.InvokeStaticMethod(typeof(Greeter), "GenericHello",
+                                                                    null, null));
+    }
+
+    // Invoke an overloaded static method with a string arg.
+    static bool TestInvokeStaticMethodWithStringArg() {
+        return CheckValue("Hello Jane",
+                          (string)VersionHandler.InvokeStaticMethod(typeof(Greeter),
+                                                                    "GenericHelloWithCustomerName",
+                                                                    new object[] { "Jane" }, null));
+    }
+
+    // Invoke an overloaded static method with an int arg.
+    static bool TestInvokeStaticMethodWithIntArg() {
+        return CheckValue("Hello customer #1337",
+                          (string)VersionHandler.InvokeStaticMethod(typeof(Greeter),
+                                                                    "GenericHelloWithCustomerName",
+                                                                    new object[] { 1337 }, null));
+    }
+
+    // Invoke a static method with a default value of a named arg.
+    static bool TestInvokeStaticMethodWithNamedArgDefault() {
+        return CheckValue("Hello There",
+                          (string)VersionHandler.InvokeStaticMethod(typeof(Greeter),
+                                                                    "GenericHelloWithPronoun",
+                                                                    null, null));
+    }
+
+    // Invoke a static method with a named arg.
+    static bool TestInvokeStaticMethodWithNamedArg() {
+        return CheckValue("Hello Miss",
+                          (string)VersionHandler.InvokeStaticMethod(
+                              typeof(Greeter), "GenericHelloWithPronoun",
+                              null, new Dictionary<string, object> { { "pronoun", "Miss" } }));
+    }
+
+    // Invoke a static method with a positional and default value for a named arg.
+    static bool TestInvokeStaticMethodWithArgAndNamedArgDefault() {
+        return CheckValue("Hello There Bob",
+                          (string)VersionHandler.InvokeStaticMethod(
+                              typeof(Greeter), "GenericHelloWithCustomerNameAndPronoun",
+                              new object[] { "Bob" }, null));
+    }
+
+    // Invoke a static method with a positional and named arg.
+    static bool TestInvokeStaticMethodWithArgAndNamedArg() {
+        return CheckValue("Hello Mrs Smith",
+                          (string)VersionHandler.InvokeStaticMethod(
+                              typeof(Greeter), "GenericHelloWithCustomerNameAndPronoun",
+                              new object[] { "Smith" },
+                              new Dictionary<string, object> { { "pronoun", "Mrs" } } ));
+    }
+
+    // Invoke an instance method with no args.
+    static bool TestInvokeInstanceMethodWithNoArgs() {
+        return CheckValue("Hello, my name is Sam",
+                          (string)VersionHandler.InvokeInstanceMethod(new Greeter("Sam"), "Hello",
+                                                                      null, null));
+    }
+
+    // Invoke an instance method with an default value for a named argument.
+    static bool TestInvokeInstanceMethodWithNamedArgDefault() {
+        return CheckValue("Hello There Helen, my name is Sam",
+                          (string)VersionHandler.InvokeInstanceMethod(
+                              new Greeter("Sam"), "HelloWithCustomerNameAndPronoun",
+                              new object[] { "Helen" }, null));
+    }
+
+    // Invoke an instance method with a named argument.
+    static bool TestInvokeInstanceMethodWithNamedArg() {
+        return CheckValue("Hello Mrs Smith, my name is Sam",
+                          (string)VersionHandler.InvokeInstanceMethod(
+                              new Greeter("Sam"), "HelloWithCustomerNameAndPronoun",
+                              new object[] { "Smith" },
+                              new Dictionary<string, object> { { "pronoun", "Mrs" } }));
+    }
+}