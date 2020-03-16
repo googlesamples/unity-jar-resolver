67455834cd48b97ea636214227bf700c78380f5b
Shawn Kuang <chkuang@google.com>
2020-03-15 19:43:24 -0700

    Improve package uninstallation feature.
    * Able to remove itself
    * Remove empty folders
    * Ignore manifest from Packages folder.
    * Add version number to the UI
    * Change menu item location.
    
    Bug: 145312206
    
    Change-Id: Id4f9e24abd2a64c9d01e75f91a95339297e4493f

--- source/VersionHandlerImpl/src/PackageUninstallWindow.cs
+++ source/VersionHandlerImpl/src/PackageUninstallWindow.cs
@@ -1,222 +1,90 @@
-﻿using System;
+﻿// <copyright file="PackageUninstallWindow.cs" company="Google LLC">
+// Copyright (C) 2020 Google LLC All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
 using UnityEditor;
 
 namespace Google {
-    [InitializeOnLoad]
-    public class PackageUninstallWindow : EditorWindow {
-        // Variables for parsing Unity package manifests
-        private const string VERSION_TOKEN = "_v*";
-        private const string FILE_EXTENSION = ".txt";
-        private const string PLAY_SERVICES_RESOLVER = "play-services-resolver";
+[InitializeOnLoad]
+public class PackageUninstallWindow : EditorWindow {
+    // Hardcoded text for the window.
+    private static string windowTitle = "Uninstall Managed Packages";
+    private static string caption =
+            "Select packages to uninstall.\n\n" +
+            "NOTE: If the files in the package have been moved, VersionHandler cannot properly "+
+            "remove moved files.";
+    private static string applylable = "Uninstall Selected Packages";
 
-        /// <summary>
-        /// Uninstall the Unity packages selected by the user
-        /// </summary>
-        [MenuItem("Assets/Play Services Resolver/Version Handler/Uninstall Package")]
-        public static void UninstallPackage() {
-            // Get list of installed packages
-            List<KeyValuePair<string, string>> displayList =
-                GetPackageList(VersionHandlerImpl.FindAllAssets());
-
-            // Remove play-services-resolver from the list
-            // TODO: Replace hard-coded package name by finding the package's assembly
-            KeyValuePair<string, string> playServicesResolver =
-                displayList.Find(kvp => kvp.Value == PLAY_SERVICES_RESOLVER);
-            displayList.Remove(new KeyValuePair<string, string>(playServicesResolver.Key,
-                                                                playServicesResolver.Value));
-
-            // Create a checkbox window listing installed packages
-            MultiSelectWindow window =
-                MultiSelectWindow.CreateMultiSelectWindow("Uninstall Packages");
-
-            // Create window event handlers
-            Action uninstallPackages = () => {
-                // Iterated through all packages checked for uninstall
-
-                RunOnMainThread.Run(() => { DeletePackages(window.SelectedItems); });
-            };
-
-            // Display MultiSelectWindow
-            window.AvailableItems = new List<KeyValuePair<string, string>>(displayList);
-            window.Caption = "Select packages to uninstall";
-            window.OnApply = uninstallPackages;
-            window.Show();
-        }
-
-        /// <summary>
-        /// Get a List of Unity packages
-        /// </summary>
-        /// <param name="assets">List of Unity assets</param>
-        /// <returns>A List of key-value pairs of filepath-PackageName</returns>
-        private static List<KeyValuePair<string, string>> GetPackageList(string[] assets) {
-            List<VersionHandlerImpl.ManifestReferences> manifestFiles = GetManifests(assets);
-            List<KeyValuePair<string, string>> installedPackages =
-                new List<KeyValuePair<string, string>>();
-            foreach (VersionHandlerImpl.ManifestReferences manifest in manifestFiles) {
-                string filepath = manifest.currentMetadata.filename;
-                string filename = Path.GetFileNameWithoutExtension(manifest.filenameCanonical);
-                installedPackages.Add(new KeyValuePair<string, string>(filepath, filename));
-            }
-
-            return installedPackages;
-        }
-
-        /// <summary>
-        /// Get a type List of currently installed Unity package manifest files. The List type
-        /// is Google.VersionHandlerImpl.ManifestReferences
-        /// </summary>
-        /// <param name="filepaths">A string array of Unity asset filepaths. The root of the
-        /// filepaths is the Unity project folder</param>
-        /// <returns>A List of manifest files, the List type is
-        /// Google.VersionHandlerImpl.ManifestReferences</returns>
-        private static List<VersionHandlerImpl.ManifestReferences>
-            GetManifests(string[] filepaths) {
-            VersionHandlerImpl.FileMetadataSet metadataSet =
-                VersionHandlerImpl.FileMetadataSet.ParseFromFilenames(filepaths);
-
-            List<VersionHandlerImpl.ManifestReferences> manifestFiles =
-                VersionHandlerImpl.ManifestReferences.FindAndReadManifests(metadataSet);
-
-            return manifestFiles;
-        }
-
-        /// <summary>
-        /// Delete a selection of installed Unity packages
-        /// </summary>
-        /// <param name="markedPackages">A HashSet of filepaths of the packages to be
-        /// uninstalled</param>
-        private static void DeletePackages(HashSet<string> markedPackages) {
-            // Get list of installed packages
-            List<KeyValuePair<string, string>> packageList =
-                GetPackageList(VersionHandlerImpl.FindAllAssets());
-
-            // Track dependent assets
-            Dictionary<string, int> manifestAssets = new Dictionary<string, int>();
-
-            // Count each manifest file as a self-dependent asset
-            foreach (KeyValuePair<string, string> package in packageList) {
-
-                // Do not track assets to be deleted for cross dependencies
-                bool skipMarked = false;
-                foreach (string markedPackage in markedPackages) {
-                    if (markedPackage == package.Key) {
-                        skipMarked = true;
-                        break;
-                    }
-                }
-                if (skipMarked) {
-                    continue;
-                }
-
-                // Track unmarked packages, for preservation
-                if (manifestAssets.ContainsKey(package.Key)) {
-                    manifestAssets[package.Key] += 1;
-                }
-                else {
-                    manifestAssets.Add(package.Key, 1);
-                }
-
-                // Track unmarked assets, for preservation
-                List<VersionHandlerImpl.ManifestReferences> manifestList =
-                    GetManifests(new string[] {package.Key});
-                foreach (VersionHandlerImpl.ManifestReferences manifest in manifestList) {
-                    foreach (string asset in manifest.currentFiles) {
-                        if (manifestAssets.ContainsKey(asset)) {
-                            manifestAssets[asset] += 1;
-                        }
-                        else {
-                            manifestAssets.Add(asset, 1);
-                        }
-                    }
+    /// <summary>
+    /// Show the window for the user to uninstall packages managed by Version Handler.
+    /// </summary>
+    [MenuItem("Assets/External Dependency Manager/Version Handler/Uninstall Managed Packages")]
+    public static void UninstallPackage() {
+        // Display MultiSelectWindow
+        MultiSelectWindow window = MultiSelectWindow.CreateMultiSelectWindow(windowTitle);
+        window.AvailableItems = GetSelectionList();
+        window.Sort(1);
+        window.Caption = caption;
+        window.ApplyLabel = applylable;
+        window.OnApply = () => {
+            if (window.SelectedItems.Count > 0) {
+                VersionHandlerImpl.ManifestReferences.DeletePackages(window.SelectedItems);
+                if (window.SelectedItems.Count == window.AvailableItems.Count) {
+                    VersionHandlerImpl.analytics.Report("uninstallpackagewindow/confirm/all",
+                            "Confirm to Uninstall All Packages");
+                } else {
+                    VersionHandlerImpl.analytics.Report("uninstallpackagewindow/confirm/subset",
+                            "Confirm to Uninstall a Subset of Packages");
                 }
             }
-
-            // Delete marked packages
-            string deletedAssets, preservedAssets;
-            DeleteIndependentAssets(markedPackages,
-                                    manifestAssets,
-                                    out deletedAssets,
-                                    out preservedAssets);
-
-            Debug.Log("Uninstalled packages\n\n" +
-                      "The following assets were deleted:\n\n" +
-                      deletedAssets + "\n\n" +
-                      "Packages were deleted successfully\n\n" +
-                      "NOTE: The following shared dependency assets were preserved:\n\n" +
-                      preservedAssets);
-        }
-
-        /// <summary>
-        /// Delete assets with no breakable dependencies
-        /// manifest
-        /// </summary>
-        /// <param name="markedPackages">A HashSet of filepaths of the packages to be
-        /// uninstalled</param>
-        /// <param name="manifestAssets">A Dictionary of assets across all manifest files.
-        /// The dictionary key is the asset file path, the value is the requency of the
-        /// asset's occurence. An asset with more than one occurrence is a breakable
-        /// dependency</param>
-        /// <param name="deletedAssets">A string of the filepaths of the deleted assets,
-        /// separated by the new line character. This parameter is passed by reference by
-        /// its caller</param>
-        /// <param name="preservedAssets">A string of the filepaths of the preserved assets,
-        /// separated by the new line character. This parameter is passed by reference by
-        /// its caller</param>
-        private static void DeleteIndependentAssets(HashSet<string> markedPackages,
-                                                    Dictionary<string, int> manifestAssets,
-                                                    out string deletedAssets,
-                                                    out string preservedAssets) {
-            deletedAssets = null;
-            preservedAssets = null;
-
-            // Iterate through manifestAssets
-            // Delete assets with 0 occurrences
-            // >= 1 occurrence means there's a cross-dependency
-            string assetDirectory = null;
-            foreach (string markedPackage in markedPackages) {
-                List<VersionHandlerImpl.ManifestReferences> manifestList =
-                    GetManifests(new string[] { markedPackage });
-                foreach (VersionHandlerImpl.ManifestReferences manifest in manifestList) {
-
-                    // Only delete manifest file if it has no dependencies
-                    manifest.currentFiles.Add(markedPackage);
-
-                    // Delete assets in the package marked for uninstall
-                    foreach (string asset in manifest.currentFiles) {
-                        if (!File.Exists(asset)) {
-                            continue; // File already deleted
-                        }
-                        if (manifestAssets.ContainsKey(asset)) {
-                            preservedAssets += asset + '\n';
-                        }
-                        else {
-                            assetDirectory = Path.GetDirectoryName(asset);
-                            AssetDatabase.MoveAssetToTrash(asset);
-                            deletedAssets += '\n' + asset;
-                            DeleteDirectory(assetDirectory);
-                        }
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Recursively deletes an empty directory and subdirectories
-        /// </summary>
-        /// <param name="packagePath">The directory path of a Unity asset</param>
-        private static void DeleteDirectory(string packagePath) {
-            string[] subDirectories = Directory.GetDirectories(packagePath);
-            foreach (string subDirectory in subDirectories) {
-                DeleteDirectory(subDirectory);
-            }
-
-            // Delete a directory only if it is empty
-            if (Directory.GetFileSystemEntries(packagePath).Length == 0) {
-                AssetDatabase.MoveAssetToTrash(packagePath);
-            }
-        }
+        };
+        window.OnCancel = () => {
+            VersionHandlerImpl.analytics.Report("uninstallpackagewindow/cancel",
+                    "Cancel to Uninstall Packages");
+        };
+        window.Show();
+        VersionHandlerImpl.analytics.Report("uninstallpackagewindow/show",
+                "Show Uninstall Package Window");
     }
+
+    /// <summary>
+    /// Get a List of packages for seleciton.
+    /// </summary>
+    /// <returns>A List of key-value pairs of canonical name to display name</returns>
+    private static List<KeyValuePair<string, string>> GetSelectionList() {
+        var manifests = VersionHandlerImpl.ManifestReferences.FindAndReadManifestsInAssetsFolder();
+        List<KeyValuePair<string, string>> selections =
+                new List<KeyValuePair<string, string>>();
+        foreach (var pkg in manifests) {
+            if (!String.IsNullOrEmpty(pkg.filenameCanonical) && pkg.metadataByVersion != null) {
+                string filename = Path.GetFileNameWithoutExtension(pkg.filenameCanonical);
+                var versions = new List<string>();
+                foreach (var fileMetadata in pkg.metadataByVersion.Values) {
+                    versions.Add(fileMetadata.versionString);
+                }
+                string displayName =
+                        String.Format("{0} version: [{1}]", filename,
+                                String.Join(", ", versions.ToArray()));
+                selections.Add(
+                        new KeyValuePair<string, string>(pkg.filenameCanonical, displayName));
+            }
+        }
+        return selections;
+    }
+}
 } // namespace Google
