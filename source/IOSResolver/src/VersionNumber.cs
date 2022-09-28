5810f4a33c9606d4495dffc891bdd7176d4ea065
drsanta@google.com <drsanta@google.com>
2022-09-28 11:56:40 -0400

    Version 1.2.173
    - Version 1.2.173 - Sep 28, 2022
    * General - added tvOS library support to the export unity package scripts.

--- source/IOSResolver/src/VersionNumber.cs
+++ source/IOSResolver/src/VersionNumber.cs
@@ -27,7 +27,7 @@ namespace Google {
         /// <summary>
         /// Version number, patched by the build process.
         /// </summary>
-        private const string VERSION_STRING = "1.2.172";
+        private const string VERSION_STRING = "1.2.173";
 
         /// <summary>
         /// Cached version structure.
