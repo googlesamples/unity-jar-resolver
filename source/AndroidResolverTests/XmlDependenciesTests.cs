85fa4e736b4a42145f4789f52168dc63e1440f09
Christopher Yarbrough <chris.yarbrough@innogames.com>
2023-03-17 17:54:18 +0100

    Add test for XmlDependencies.IsDependenciesFile
    This commit records the current behaviour of the method before I make changes to try and optimize it.

---
+++ source/AndroidResolverTests/XmlDependenciesTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GooglePlayServices.Tests {
+	using System;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class XmlDependenciesTests
+	{
+		[TestCase("Assets/Editor/Dependencies.xml")]
+		[TestCase("Assets/Editor/MyFolder/Dependencies.xml")]
+		[TestCase("Editor/Dependencies.xml")]
+		[TestCase("Editor/MyFolder/Dependencies.xml")]
+		[TestCase("Assets/Editor/SomeDependencies.xml")]
+		[TestCase("Assets/MyEditorCode/Dependencies.xml")]
+		[TestCase("Assets/MyEditorCode/SomeDependencies.xml")]
+		[TestCase("Assets/Editor/")]
+		[TestCase("Assets/Editor/Dependendencies")]
+		public void Test1(string path) {
+			var dependencies = new XmlDependencies();
+			bool actualResult = dependencies.IsDependenciesFile(path);
+
+			// This logic was part of the previous unoptimized implementation and can act as a test reference.
+			bool expectedResult = Regex.IsMatch(input: path, pattern: @".*[/\\]Editor[/\\].*Dependencies\.xml$");
+
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+	}
+}
