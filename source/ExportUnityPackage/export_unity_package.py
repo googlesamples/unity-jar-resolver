a9636f72cf6617b4b119b0b0a53a89876debf482
chkuang-g <31869252+chkuang-g@users.noreply.github.com>
2023-09-15 15:33:24 +0800

    Prevent package from including Mac Metadata file (#640)
    * Prevent package from including Mac Metadata file
    
    If `tar` is run from a macOS, by default, it will create Mac Metadata
    file for every file and folder which starts with ".". This change makes
    sure that the archives does not include Mac Metadata file.

--- source/ExportUnityPackage/export_unity_package.py
+++ source/ExportUnityPackage/export_unity_package.py
@@ -2595,6 +2595,9 @@ class PackageConfiguration(ConfigurationBlock):
                 os.utime(filename, (FLAGS.timestamp, FLAGS.timestamp))
             # Don't recurse directories.
             tar_args.append("-n")
+            # Avoid creating mac metadata files with name started with "."
+            if platform.system() == "Darwin":
+              tar_args.append("--no-mac-metadata")
           tar_args.extend(["-T", list_filename])
           # Disable timestamp in the gzip header.
           tar_env = os.environ.copy()
